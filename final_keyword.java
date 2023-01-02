05ec6db73a47ebceb4ac7d770703c9d06c997e96
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2023-01-03 01:26:04 +0530

    Create final_keyword.java

---
+++ final_keyword.java
@@ -0,0 +1,11 @@
+class Pen{
+    final int price = 15;
+}
+
+public class MCQs {
+    public static void main(String[] args) {
+        Pen p = new Pen();
+        p.price = 20;
+        System.out.println(p.price);
+    }   
+}  
