23c1fdf244b36290f8611524521c9dad2329da78
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2023-01-03 01:35:11 +0530

    Create static_function.java

---
+++ static_function.java
@@ -0,0 +1,19 @@
+class Test 
+{ 
+    static int a = 10; 
+    static int b; 
+    static void fun(){ 
+        b = a * 4; 
+    } 
+} 
+class MCQ{
+   public static void main(String[] args) 
+    { 
+      Test t=new Test();
+      t.fun();
+      System.out.print(Test.a+Test.b);
+    } 
+}
+
+//output 
+//50
